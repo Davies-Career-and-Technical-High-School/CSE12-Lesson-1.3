a50a911f0f5c86901a9ed5900574673f2e72996f
Joe Mazzone <jmazzone@daviestech.org>
2020-10-02 02:23:10 +0000

    Initial commit

---
+++ Lesson1_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+
+    //Data types - int, double, String
+    int x = 20;
+            
+    System.out.println(x);
+    System.out.println(x * 7);
+
+  }
+}
